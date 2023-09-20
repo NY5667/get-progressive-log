c703e276c4818cd6088fd2a6b8f83392a9253e73
NY5667 <82657677+NY5667@users.noreply.github.com>
2023-09-20 14:03:56 +0800

    update

--- exit.bat
+++ exit.bat
@@ -1,10 +1,10 @@
 @echo off
 
 REM 激活虚拟环境
-call ..\venv\Scripts\activate
+call venv\Scripts\activate
 
 REM 运行 Python 脚本
-python ms_log.py
+python delete_online_user.py
 
 REM 关闭虚拟环境
 REM deactivate
